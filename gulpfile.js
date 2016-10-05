dc6f9115cf0b07778a58194bba163515350938ad
jordanbrauer <jbrauer.inc@gmail.com>
2016-10-05 00:33:12 -0500

    Completed conversion commit.

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,10 @@ var gulp          = require('gulp'),               // task runner/manager (even 
     notify        = require('gulp-notify'),        // notifies us when a gulp task is completed through a system notifcation
     rename        = require('gulp-rename'),        // allows us to rename files (adding prefixes and suffixes is the main use here).
     autoprefixer  = require('gulp-autoprefixer'),  // great for catching those annoying vendor prefixes on css attributets and values.
-    minify        = require('gulp-cssnano'),       // minify our compiled css. sass can do this natively (kind of), so this is used in conjunction with gulp-sass.
-    del           = require('del');                // del is used to cleanup cache and build files.
+    minify        = require('gulp-cssnano'),       // minify our compiled css. sass can do this natively (kind of), but this is used instead.
+    del           = require('del'),                // del is used to cleanup cache and build files.
+    runseq        = require('run-sequence'),       // quick and dirty way to run a sequence of tasks AS a task itself. gulp.run is deprecated
+    merge         = require('merge');              // "merge multiple objects into one, optionally creating a new cloned object." -npm repo
 
 /*
  * Variables ====================================
@@ -32,7 +34,7 @@ var $supported = [
 
 /*
  * Build CSS Task -------------------------------
- * | $ gulp build
+ * | $ gulp build:css
  * |
  * | 1. compiles scss into css,
  * | 2. autoprefix necessary css attributes/values,
@@ -42,38 +44,64 @@ var $supported = [
  * | 6. place minified file into ./css directory
  * | 7. notify on task completion
  */
-gulp.task('build', function() {
+gulp.task('build:css', function() {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass({              // 1.
+    .pipe(sass({              /* 1. */
       outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(autoprefixer({      // 2.
+    .pipe(autoprefixer({      /* 2. */
       browsers: $supported,
       add: true,
       cascade: false
     }))
-    .pipe(gulp.dest('./css')) // 3.
-    .pipe(minify({            // 4.
+    .pipe(gulp.dest('./css')) /* 3. */
+    .pipe(minify({            /* 4. */
       discardUnused: { fontFace: false }
     }))
-    .pipe(rename({            // 5.
+    .pipe(rename({            /* 5. */
       suffix: '.min'
     }))
-    .pipe(gulp.dest('./css')) // 6.
-    .pipe(notify({            // 7.
+    .pipe(gulp.dest('./css')) /* 6. */
+    .pipe(notify({             /* 7. */
       onLast: true,
       message: 'build task complete!'
     }));
 });
 
-/*
- * Clean Task -----------------------------------
+/** Create Config -------------------------------
+ * | $ gulp gen:configFile
+ */
+gulp.task('gen:config', function() {
+  return gulp.src('./sass/config/_variables.scss')
+    .pipe(rename('config/_settings.scss'))
+    .pipe(gulp.dest('./sass'))
+    .pipe(notify({
+      onLast: true,
+      message: 'generated config file successfully!'
+    }));
+});
+
+/** Clean Task ----------------------------------
  * | $ gulp clean
  * |
  * | Ensures that all artifacts/remnants of previous build are gone.
  */
 gulp.task('clean', function() {
-  return del(['./css/**/*.*']);
+  var stream1 = del(['./css/**/*.*']);
+  var stream2 = del(['./sass/config/_settings.scss']);
+
+  return merge(stream1, stream2);
+});
+
+/** Build Task -------------------------------
+ * | $ gulp build
+ */
+gulp.task('build', function(){
+  return runseq(
+    'clean',
+    'build:css',
+    'gen:config'
+  );
 });
 
 /*
@@ -83,7 +111,7 @@ gulp.task('clean', function() {
  * | Great for automating compilation during develpoment.
  */
 gulp.task('watch', function() {
-  gulp.watch('./sass/**/*.scss', ['clean', 'build']);
+  gulp.watch('./sass/**/*.scss', ['build']);
 });
 
 /*
